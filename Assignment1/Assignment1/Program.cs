9aeeaaf7755ba49bbfe84954665a40270bdeaf66
Halina <galaratkova@gmail.com>
2019-11-07 12:12:26 -0500

    first commit

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] myArray = {1,2,3,3,3,4,5};
+            int[] temp = new int[myArray.Length];
+            Array.Sort(myArray);
+            int j = 0;
+            for (int i = 0; i < myArray.Length-1; i++){
+                if (myArray[i] != myArray[i+1])
+                {
+                    temp[j++]= myArray[i];
+                }
+            }
+            temp[j] = myArray[myArray.Length - 1];
+            for (int k = 0; k <= j; k++){
+                Console.Write("{0}", temp[k]);
+   
+            }
+            Console.ReadKey();
+        }
+    } 
+}
+
